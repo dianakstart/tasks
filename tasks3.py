1bfde1537c159c8c620860c1b57dc1cfc7069f3d
dianakstart <dianakstart@gmail.com>
2026-02-07 13:19:36 +0300

    refactor: reimplement program to read three numbers, validate their range, and print the maximum or an error.

--- tasks3.py
+++ tasks3.py
@@ -1,2 +1,11 @@
-a = int(input())
-print(a**2)
+M1, M2, M3 = input().split()
+M1 = int(M1)
+M2 = int(M2)
+M3 = int(M3)
+Mmax = max(M1, M2, M3)
+Mmin = min(M1, M2, M3)
+
+if 94 <= Mmin and  Mmax <= 727:
+    print(Mmax)
+else:
+    print("Error")
